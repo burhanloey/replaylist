4aaf73f7a97343cfb9744ed913ac0b67a8961c36
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-06-10 11:26:19 +0800

    Minify javascript file (#1)

--- load_iframe_api.js
+++
@@ -1,5 +0,0 @@
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
